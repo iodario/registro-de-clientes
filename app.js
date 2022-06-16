51a804cc81ae4284dd99f0b6c6ac3052e20cf6ee
Dario Turchi <iodariomdq@outlook.com>
2022-06-15 22:09:03 -0300

    storage

--- app.js
+++ app.js
@@ -1,42 +1,69 @@
 const personas = [];
-
-
 let formulario = document.getElementById("formulario");
 
 class Persona {
-  constructor(literal) {
-    this.nombre = literal.nombre;
-    this.apellido = literal.apellido;
-    this.email = literal.email;
-    this.edad = literal.edad;
-    this.dni = literal.dni;
-   
+  constructor(nombre, apellido, email, edad, dni) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.email = email;
+    this.edad = edad;
+    this.dni = dni;
+  }
 
+  static crearPersona(persona) {
+    let p = new Persona(persona.nombre, persona.apellido, persona.email, persona.edad, persona.dni);
+    return p;
   }
 }
-
-
 
 
 formulario.onsubmit = (e) => {
 
   e.preventDefault();
   const inputs = e.target;
-  personas.push
-    (new
-      Persona({ nombre: inputs[0].value, 
-        apellido: inputs[1].value, 
-        email: inputs[2].value, 
-        edad: inputs[3].value, 
-        dni: inputs[4].value
-        }));
+    let nombre = inputs[0].value;
+    let apellido = inputs[1].value;
+    let email = inputs[2].value;
+    let edad = inputs[3].value;
+    let dni = inputs[4].value;
 
-  mostrarPersonas(personas);
-  alert("turno reservado con exito");
+    let nueva_persona = new Persona(nombre, apellido, email, edad, dni);
+
+      
+  
+  guardar_personas(nueva_persona);
+
+  mostrarPersonas();
+
+  alert("cliente guardado con exito");
+}
+
+const clave_lista_storage = "listaPersonas";
+function guardar_personas(persona_nueva) {
+
+  let item = localStorage.getItem(clave_lista_storage);
+  if (item) {
+
+    let almacenados = JSON.parse(localStorage.getItem(clave_lista_storage));
+    almacenados.push(persona_nueva);
+
+    let almacenados_string = JSON.stringify(almacenados);
+    localStorage.setItem(clave_lista_storage, almacenados_string);
+
+  } else {
+
+    let almacenados = new Array();
+    almacenados.push(persona_nueva);
+    let almacenados_string = JSON.stringify(almacenados);
+    localStorage.setItem(clave_lista_storage, almacenados_string);
+
+
+  }
 }
 
 
-function mostrarPersonas(personas) {
+
+function mostrarPersonas() {
 
   salida.innerHTML = `
     <table class="table">
@@ -48,27 +75,66 @@ function mostrarPersonas(personas) {
       <th scope="col">Edad</th>
       <th scope="col">Dni</th>
     </tr>
-  </thead>  
-  
- 
+  </thead>   
     `;
 
-  for (const persona of personas) {
-    let trPersona = document.createElement("div");
-    trPersona.innerHTML = `<tbody>
+  //recorrer arreglo storage
+  let almacenados = JSON.parse(localStorage.getItem(clave_lista_storage));
+  for (let i = 0; i < almacenados.length; i++) {
+    let persona = almacenados[i];
+    salida.innerHTML += `
+    <tbody>
     <tr>
-    <th scope="row">El nombre es: ${persona.nombre}</th>
-    <td>El apellido es: ${persona.apellido}</td>
-    <td>El email es: ${persona.email}</td>
-    <td>La edad es: ${persona.edad}</td>
-    <td>El dni es: ${persona.dni}</td>
+      <td>${persona.nombre}</td>
+      <td>${persona.apellido}</td>
+      <td>${persona.email}</td>
+      <td>${persona.edad}</td>
+      <td>${persona.dni}</td>
     </tr>
-  `
-    salida.appendChild(trPersona);
+  </tbody>
+    `;
   }
-  `</tbody>
-    </table>`
+  return salida.innerHTML;
 }
+
+  // let arreglo = localStorage.getItem(clave_lista_storage);
+  // if (arreglo) {
+
+  //   arreglo = JSON.parse(arreglo);
+  //   for (let i = 0; i < arreglo.length; i++) {
+
+  //     let persona = arreglo[i];
+  //     let nueva_persona = Persona.crearPersona(persona);
+  //     console.log(nueva_persona);
+  //     console.log(nueva_persona.nombre);
+
+
+  //   }
+  //   return arreglo;
+  // }
+
+  
+
+
+
+  // for (const persona of personas) {
+  //   let trPersona = document.createElement("div");
+  //   trPersona.innerHTML = `<tbody>
+  //   <tr>
+  //   <th scope="row">El nombre es: ${persona.nombre}</th>
+  //   <td>El apellido es: ${persona.apellido}</td>
+  //   <td>El email es: ${persona.email}</td>
+  //   <td>La edad es: ${persona.edad}</td>
+  //   <td>El dni es: ${persona.dni}</td>
+
+  //   </tr>
+  // `
+  //   //<td>El turno es: ${persona.turno}</td>
+  //   salida.appendChild(trPersona);
+  // }
+  // `</tbody>
+  //   </table>`
+
 const salida = document.createElement("div");
 document.body.appendChild(formulario);
 document.body.appendChild(salida);
